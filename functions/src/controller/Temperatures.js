dce68fb430ca0560bb6f709c228eed7670338db1
Zharif Othman <zharifothman92@gmail.com>
2018-10-17 01:58:47 +0800

    add get method endpoint

--- functions/src/controller/Temperatures.js
+++ functions/src/controller/Temperatures.js
@@ -2,6 +2,15 @@ const admin = require("firebase-admin");
 // const uuidV1 = require('uuid/v1');
 
 module.exports = {
+  findAll: (req, res) => {
+    const ref = admin.database().ref('temperatures');
+    ref.on('value', (snapshot) => {
+      const notes = snapshot.val();
+      const notesArray = Object.keys(notes).map(i => notes[i])
+      return res.json(notesArray);
+    });
+  },
+
   create: (req, res) => {
     // if (!req.body.text || req.body.text.length < 3) return res.status(400).send('Text is required and should be minimum 3 character');
     // if (!req.body.date) return res.status(400).send('Date is required')
@@ -20,7 +29,7 @@ module.exports = {
     }
 
     const temperatures = {
-      id: makeid(),
+      // id: makeid(),
       temperature: req.body.temperature,
       timestamp: req.body.timestamp,
     };
--- functions/src/routes/api/temperature.js
+++ functions/src/routes/api/temperature.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const TemperatureController = require('../../controller/Temperatures');
 
 router.route('/temperatures')
-  // .get(TemperatureController.findAll)
+  .get(TemperatureController.findAll)
   .post(TemperatureController.create);
 
 module.exports = router;
