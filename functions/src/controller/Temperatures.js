c17537b90540a68797280cc337c708430bb855ac
Zharif Othman <zharifothman92@gmail.com>
2018-10-20 12:57:11 +0800

    change store id variable

--- functions/src/controller/Temperatures.js
+++ functions/src/controller/Temperatures.js
@@ -15,26 +15,15 @@ module.exports = {
     // if (!req.body.text || req.body.text.length < 3) return res.status(400).send('Text is required and should be minimum 3 character');
     // if (!req.body.date) return res.status(400).send('Date is required')
 
-    const ref = admin.database().ref('temperatures');
-
-    // Function makeid() use for generate Random Unique ID
-    function makeid() {
-      var text = "";
-      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-      for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-      return text;
-    }
+    const ref = admin.database().ref('temperatures').push();
 
     const temperatures = {
-      // id: makeid(),
+      id: ref.getKey(),
       temperature: req.body.temperature,
       timestamp: req.body.timestamp,
     };
 
-    ref.push(temperatures);
+    ref.set(temperatures);
 
     const response = {
       message: 'created',
