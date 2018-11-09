c1bd70981ab2af1dc75afb5bec40ebbdefb34e41
Zharif Othman <zharifothman92@gmail.com>
2018-11-09 17:14:27 +0800

    [NPM install] add date now and push notifcation

--- functions/src/controller/Temperatures.js
+++ functions/src/controller/Temperatures.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-// const uuidV1 = require('uuid/v1');
+var request = require('request');
 
 module.exports = {
   findAll: (req, res) => {
@@ -16,11 +16,48 @@ module.exports = {
     // if (!req.body.date) return res.status(400).send('Date is required')
 
     const ref = admin.database().ref('temperatures').push();
+    const temperature = req.body.temperature;
+    
+    if (temperature > 30 && temperature < 33) {
+      const bodyObject = { 
+        "to": "ExponentPushToken[7bJ8PMJ6Cr7hpjwOVgdo14]",
+        "title": "Warning Notification",
+        "body": "Get Your Car for Checkup!!"
+      };
+
+      request({
+          url: "https://exp.host/--/api/v2/push/send",
+          method: "POST",
+          json: true,   // <--Very important!!!
+          // header: headerObject,
+          body: bodyObject
+      }, function (error, response, body){
+          console.log(response);
+      });
+    } 
+    
+    if (temperature >= 33 ) {
+      const bodyObject = { 
+        "to": "ExponentPushToken[7bJ8PMJ6Cr7hpjwOVgdo14]",
+        "title": "Danger Notification",
+        "body": "Stop Your Car NOW!!"
+      };
+
+      request({
+          url: "https://exp.host/--/api/v2/push/send",
+          method: "POST",
+          json: true,   // <--Very important!!!
+          // header: headerObject,
+          body: bodyObject
+      }, function (error, response, body){
+          console.log(response);
+      });
+    }
 
     const temperatures = {
       id: ref.getKey(),
       temperature: req.body.temperature,
-      timestamp: req.body.timestamp,
+      timestamp: Date.now(),
     };
 
     ref.set(temperatures);
