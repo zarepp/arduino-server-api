fc52f5fb6738fa87e71463bab1b8ad283221a1e1
Zharif Othman <zharifothman92@gmail.com>
2018-11-13 23:43:25 +0800

    refactor notification

--- functions/src/controller/Temperatures.js
+++ functions/src/controller/Temperatures.js
@@ -1,9 +1,10 @@
 const admin = require("firebase-admin");
-var request = require('request');
+
+const NotificationService = require('../services/Notifications');
 
 module.exports = {
   findAll: (req, res) => {
-    const ref = admin.database().ref('temperatures');
+    const ref = admin.database().ref('temperatures').limitToLast(10);
     ref.on('value', (snapshot) => {
       const notes = snapshot.val();
       const notesArray = Object.keys(notes).map(i => notes[i])
@@ -12,50 +13,22 @@ module.exports = {
   },
 
   create: (req, res) => {
-    // if (!req.body.text || req.body.text.length < 3) return res.status(400).send('Text is required and should be minimum 3 character');
-    // if (!req.body.date) return res.status(400).send('Date is required')
-
     const ref = admin.database().ref('temperatures').push();
-    const temperature = req.body.temperature;
-    
-    if (temperature > 30 && temperature < 33) {
-      const bodyObject = { 
-        "to": "ExponentPushToken[7bJ8PMJ6Cr7hpjwOVgdo14]",
-        "title": "Warning Notification",
-        "body": "Get Your Car for Checkup!!"
-      };
+    const { temperature, arduinoId } = req.body;
 
-      request({
-          url: "https://exp.host/--/api/v2/push/send",
-          method: "POST",
-          json: true,   // <--Very important!!!
-          // header: headerObject,
-          body: bodyObject
-      }, function (error, response, body){
-          console.log(response);
-      });
+    // warning notification
+    if (temperature > 30 && temperature < 33) {
+      NotificationService.warning(arduinoId);
     } 
     
+    // danger notification
     if (temperature >= 33 ) {
-      const bodyObject = { 
-        "to": "ExponentPushToken[7bJ8PMJ6Cr7hpjwOVgdo14]",
-        "title": "Danger Notification",
-        "body": "Stop Your Car NOW!!"
-      };
-
-      request({
-          url: "https://exp.host/--/api/v2/push/send",
-          method: "POST",
-          json: true,   // <--Very important!!!
-          // header: headerObject,
-          body: bodyObject
-      }, function (error, response, body){
-          console.log(response);
-      });
+      NotificationService.danger(arduinoId);
     }
 
     const temperatures = {
       id: ref.getKey(),
+      arduinoId: req.body.arduinoId,
       temperature: req.body.temperature,
       timestamp: Date.now(),
     };
@@ -67,7 +40,5 @@ module.exports = {
       status: '201'
     }
     return res.json(response);
-  },
+  }
 }
-
-
